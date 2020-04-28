d03bf1e2a1bea34ab1c68b2a3db27c3ef201e936
simon <simon.mettag@gmail.com>
2020-04-27 21:45:02 -0500

    Componentes base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,24 @@ import { AppComponent } from './app.component';
 import { BalanceComponent } from './balance/balance.component';
 import { NavComponent } from './nav/nav.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FootComponent } from './foot/foot.component';
+import { InfoTableComponent } from './info-table/info-table.component';
+import { SendFormComponent } from './send-form/send-form.component';
+import { WithFormComponent } from './with-form/with-form.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ActionButtonsComponent } from './action-buttons/action-buttons.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BalanceComponent,
-    NavComponent
+    NavComponent,
+    FootComponent,
+    InfoTableComponent,
+    SendFormComponent,
+    WithFormComponent,
+    SettingsComponent,
+    ActionButtonsComponent
   ],
   imports: [
     BrowserModule,
