05dfa9deb03cd2cf12f815c2b4ee1b9763de93a4
Luis <luisrevilla20@itesm.mx>
2020-05-11 19:28:58 -0500

    Admin Transaction Service

--- src/app/modules/admin/models/admin/admin.ts
+++ src/app/modules/admin/models/admin/admin.ts
@@ -1,6 +1,16 @@
 export class Admin {
+<<<<<<< Updated upstream
 	constructor(public name?: string, 
 		public balance?: Number
 		){
 	}
+=======
+    constructor(public username?: string, 
+                public password?: string, 
+                public email?: string,
+                public balance?: Number,
+                ){
+
+    }
+>>>>>>> Stashed changes
 }
--- src/app/modules/admin/services/admin/admin.service.ts
+++ src/app/modules/admin/services/admin/admin.service.ts
@@ -5,11 +5,21 @@ import { Admin } from '../../models/admin/admin';
 @Injectable({
   providedIn: 'root'
 })
+<<<<<<< Updated upstream
 export class AdminService {
 	getAdmin(): Observable<Admin>{
 		let admin :Admin = new Admin ('Admin Simon', 100000);
 		return of(admin)
 	  }
+=======
+export class UserService {
+
+  getUser(): Observable<Admin[]>{
+    let users :Admin[] = [new Admin ('Luis', '123', 'luis@gmail.com', 500000),
+                         ]
+    return of(users)
+  }
+>>>>>>> Stashed changes
 
   constructor() { }
 }
