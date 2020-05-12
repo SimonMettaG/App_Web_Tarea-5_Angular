25965df61ed6eedecec311d977c0d6ce3da3dbb0
Luis <luisrevilla20@itesm.mx>
2020-05-11 19:42:01 -0500

    Services Completed

--- src/app/modules/admin/models/admin/admin.ts
+++ src/app/modules/admin/models/admin/admin.ts
@@ -1,16 +1,6 @@
 export class Admin {
-<<<<<<< Updated upstream
 	constructor(public name?: string, 
 		public balance?: Number
 		){
 	}
-=======
-    constructor(public username?: string, 
-                public password?: string, 
-                public email?: string,
-                public balance?: Number,
-                ){
-
-    }
->>>>>>> Stashed changes
 }
